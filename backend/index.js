c88a3ea52f4809efee8dad0aeac6f85392adc3b9
Claudia Perez <perezvelazquezclaudia@gmail.com>
2024-09-15 23:54:20 -0300

    aplicar variable PORT

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,11 @@
 
 const app = require('./src/app');
-const PORT = process.env.PORT || 3000;
+require('dotenv').config();
+
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+
